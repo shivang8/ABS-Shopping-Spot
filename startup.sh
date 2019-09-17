f131d9fe40211103d1ec6931f79dbe3897eccf7b
shivang8 <satyamsri8@gmail.com>
2019-09-17 22:43:28 +0530

    Changes

--- startup.sh
+++ startup.sh
@@ -6,6 +6,6 @@ sudo pip install flask-sqlalchemy
 sudo apt install libpq-dev python-dev
 sudo pip install psycopg2
 sudo pip install pymysql
-sudo git clone https://github.com/shivang8/flask007.git
-cd flask007/
+sudo git clone https://github.com/shivang8/ABS-Shopping-Spot.git
+cd ABS-Shopping-Spot/
 sudo python server.py & disown
